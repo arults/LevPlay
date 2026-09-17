f32280ad31d2dfca21c12595784713d46e292dcf
arults <109891188+arults@users.noreply.github.com>
2026-09-17 08:13:27 +0530

    Type account descriptor array explicitly

--- programs/levplay-sbf/src/open_validation.rs
+++ programs/levplay-sbf/src/open_validation.rs
@@ -78,7 +78,7 @@ pub fn validate_open_account_set(
         instructions_sysvar: sysvar::instructions::ID.to_bytes(),
     };
 
-    let descriptors = core::array::from_fn(|index| descriptor(&accounts[index]));
+    let descriptors: [AccountDescriptor; OPEN_ACCOUNT_COUNT] =\n        core::array::from_fn(|index| descriptor(&accounts[index]));
     validate_descriptor_set(&bindings, &descriptors)
         .map_err(|_| ProgramError::InvalidAccountData)?;
 
